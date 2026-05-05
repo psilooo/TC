29c8ca379ed524148b2a68402ee4f37ac1837a74
psilooo <03.kylewis@gmail.com>
2026-05-05 16:35:45 -0400

    feat(contacts): compose Contacts page

--- src/pages/Contacts.vue
+++ src/pages/Contacts.vue
@@ -1,7 +1,40 @@
+<!-- src/pages/Contacts.vue -->
 <template>
-  <PagePlaceholder name="Contacts" />
+  <div class="page page--contacts">
+    <PageHeader title="Contacts" />
+
+    <KpiRow :kpis="contactsKpis" />
+
+    <section class="page--contacts__body">
+      <ContactsTable :contacts="contactsDirectory" :selected-id="contactsSelectedId" :total="contactsKpis[0].value" />
+      <ContactDetailRail :contact="contactsSelectedContact" />
+    </section>
+  </div>
 </template>
 
 <script setup lang="ts">
-import PagePlaceholder from '../components/PagePlaceholder.vue'
+import PageHeader from '../components/PageHeader.vue'
+import KpiRow from '../components/KpiRow.vue'
+import ContactsTable from '../components/contacts/ContactsTable.vue'
+import ContactDetailRail from '../components/contacts/ContactDetailRail.vue'
+import { contactsKpis, contactsDirectory, contactsSelectedId, contactsSelectedContact } from '../data/contacts'
 </script>
+
+<style scoped lang="scss">
+.page--contacts {
+  padding: 1.5rem;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+}
+.page--contacts__body {
+  display: grid;
+  grid-template-columns: 1fr;
+  gap: 1rem;
+}
+@media (min-width: 1024px) {
+  .page--contacts__body {
+    grid-template-columns: 2fr 1fr;
+  }
+}
+</style>
