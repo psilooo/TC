5e4a01383ddca4276eba27e8e2bfc5c4fc1420fd
psilooo <03.kylewis@gmail.com>
2026-05-05 12:00:30 -0400

    feat(navbar): add NavbarSearch component

---
+++ src/components/navbar/components/NavbarSearch.vue
@@ -0,0 +1,19 @@
+<!-- src/components/navbar/components/NavbarSearch.vue -->
+<template>
+  <VaInput
+    class="navbar-search"
+    placeholder="Search shows, dates, venues, contacts, tasks, and more…"
+    aria-label="Global search"
+  >
+    <template #prependInner>
+      <VaIcon name="mso-search" color="secondary" />
+    </template>
+  </VaInput>
+</template>
+
+<style scoped lang="scss">
+.navbar-search {
+  min-width: 16rem;
+  flex: 1 1 auto;
+}
+</style>
